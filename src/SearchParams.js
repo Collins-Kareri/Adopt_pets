e6676667faf6846746bf20b628ddbac547b45b66
Collins-Kareri <locoreri@gmail.com>
2020-03-02 10:03:55 +0300

    Learning frontend

---
+++ src/SearchParams.js
@@ -0,0 +1,24 @@
+import React,{ useState } from 'react';
+
+const SearchParams = () => {
+    //replace location
+    const [location,updateLocation] = useState("Seattle, WA")
+    return (
+        <div className="search-params">
+            <form>
+                <label htmlFor="location">
+                    location
+                    <input id="location"
+                        value={ location }
+                        placeholder="Location"
+                        onChange={ (e) => updateLocation
+                            (e.target.value) }
+                    />
+                </label>
+                <button>Submit</button>
+            </form>
+        </div>
+    );
+};
+
+export default SearchParams;
